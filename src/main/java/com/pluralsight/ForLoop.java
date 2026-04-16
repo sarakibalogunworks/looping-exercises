ae0a9008438e7f91671f17eec0b69cfb584e436d
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-16 12:31:28 -0400

    finished for loop commit

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int n = 10; n>= 1; n = n -1){
+            System.out.println(n);
+            Thread.sleep(1000);
+        }
+        System.out.println("Launch");
+    }
 }
