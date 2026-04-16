a39f0906f6b4842a4141ca70190ab383e58ac7aa
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-16 12:32:50 -0400

    added some edits to my conditions commit

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -3,10 +3,10 @@ package com.pluralsight;
 public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int n = 10; n>= 1; n = n -1){
+        for (int n = 10; n >= 1; n = n - 1){
             System.out.println(n);
             Thread.sleep(1000);
         }
-        System.out.println("Launch");
+        System.out.println("Launch!");
     }
 }
