f60b9c6e99fac47d242e5b2fcc002be76462193b
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-16 12:24:42 -0400

    finished do while loop commit

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        do {
+            System.out.println("I Love Java");
+            number++;
+        } while (number < 5);
+    }
 }
