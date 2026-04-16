98e9fefd4cacd4efffefa4fd9b324bf112d4e241
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-16 12:19:19 -0400

    finished while loop commit

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class WhileLoop {
+
+    public static void main(String[] args) {
+
+         int counter = 0;
+
+         while (counter < 5){
+             System.out.println("I Love Java");
+             counter++;
+         }
+    }
 }
